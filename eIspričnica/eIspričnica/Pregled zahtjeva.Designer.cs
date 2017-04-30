c6760ae2ca46d37b2e91e85a5e427a7dd93f35d4
Denis Maljak <denmaljak@foi.hr>
2017-04-30 22:01:22 +0200

    Pregled zahtjeva
    Dodana forma i napravljen početni dizajn.

---
+++ eIspričnica/eIspričnica/Pregled zahtjeva.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace eIspričnica
+{
+    partial class Pregled_zahtjeva
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listaZahtjeva = new System.Windows.Forms.ListBox();
+            this.odabirZahtjeva = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listaZahtjeva
+            // 
+            this.listaZahtjeva.FormattingEnabled = true;
+            this.listaZahtjeva.Location = new System.Drawing.Point(53, 35);
+            this.listaZahtjeva.Name = "listaZahtjeva";
+            this.listaZahtjeva.Size = new System.Drawing.Size(352, 472);
+            this.listaZahtjeva.TabIndex = 0;
+            // 
+            // odabirZahtjeva
+            // 
+            this.odabirZahtjeva.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.odabirZahtjeva.Location = new System.Drawing.Point(535, 461);
+            this.odabirZahtjeva.Name = "odabirZahtjeva";
+            this.odabirZahtjeva.Size = new System.Drawing.Size(125, 46);
+            this.odabirZahtjeva.TabIndex = 1;
+            this.odabirZahtjeva.Text = "Odabir";
+            this.odabirZahtjeva.UseVisualStyleBackColor = true;
+            // 
+            // Pregled_zahtjeva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 541);
+            this.Controls.Add(this.odabirZahtjeva);
+            this.Controls.Add(this.listaZahtjeva);
+            this.Name = "Pregled_zahtjeva";
+            this.Text = "Pregled_zahtjeva";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listaZahtjeva;
+        private System.Windows.Forms.Button odabirZahtjeva;
+    }
+}
---
+++ eIspričnica/eIspričnica/Pregled zahtjeva.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eIspričnica
+{
+    public partial class Pregled_zahtjeva : Form
+    {
+        public Pregled_zahtjeva()
+        {
+            InitializeComponent();
+        }
+    }
+}
